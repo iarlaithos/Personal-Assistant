398fda8958f4742aa6686393e11b9cd2a80001c0
iarlaithos <67965052+iarlaithos@users.noreply.github.com>
2023-01-19 17:31:11 +0000

    Upon register, user obj is created in DB. User can sign in using DB Auth, auth is then saved to shared pref.

--- app/src/main/java/com/iarlaith/personalassistant/HomePageActivity.kt
+++ app/src/main/java/com/iarlaith/personalassistant/HomePageActivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomePageActivity : AppCompatActivity() {
+
+    private lateinit var fireAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
         val menu = findViewById<ImageButton>(R.id.btnMenu)
+        fireAuth = Firebase.auth
 
         menu.setOnClickListener {
+            println("######")
+            println(fireAuth.currentUser?.uid)
+            println("######")
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
---
+++ app/src/main/java/com/iarlaith/personalassistant/ModulesMenu.kt
@@ -0,0 +1,11 @@
+package com.iarlaith.personalassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModulesMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modules_menu)
+    }
+}
